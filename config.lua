ec67eeebaa1bc46cfb8e5146a3cb33d9f0aa4a04
JonarSonaer <haikara98@gmail.com>
2022-03-05 01:03:56 +0100

    1.2

--- config.lua
+++ config.lua
@@ -4,12 +4,14 @@ Config.MarkerColor                = { r = 255, g = 255, b = 255 }
 Config.Locale                     = 'sv'
 Config.Price = 2 -- det här är gånger hur mycket det ska visa
 Config.Zones = {
+  
   ShopInside = {
     Pos     = { x = -54.6 , y = -1096.9, z = 26.3 },
     Size    = { x = 1.5, y = 1.5, z = 1.0 },
     Heading = 325.28,
     Type    = -1,
   },
+  
   Katalog = {
     Pos     = { x = -51.09 , y = -1094.74, z = 26.3 },
     Size    = { x = 1.5, y = 1.5, z = 1.0 },
